9a4fce52bedc6d9c59d5de78cee46c7de2659c6e
djm2x <dj-m2x@hotmail.com>
2020-07-27 17:58:37 +0100

    commit

--- angular/src/app/services/axe.service.ts
+++ angular/src/app/services/axe.service.ts
@@ -1,10 +1,11 @@
+import { Axe } from './../Models/models';
 import { Injectable } from '@angular/core';
 import { SuperService } from './super.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AxeService  extends SuperService<any> {
+export class AxeService  extends SuperService<Axe> {
 
   constructor() {
     super('axes');
--- angular/src/app/services/sous.axe.service.ts
+++ angular/src/app/services/sous.axe.service.ts
@@ -1,10 +1,11 @@
+import { SousAxe } from './../Models/models';
 import { Injectable } from '@angular/core';
 import { SuperService } from './super.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SousAxeService  extends SuperService<any> {
+export class SousAxeService  extends SuperService<SousAxe> {
 
   constructor() {
     super('sousAxes');
