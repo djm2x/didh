83451bc3e2adace934ee04b5e0aeb411bc1f8d2d
djm2x <dj-m2x@hotmail.com>
2021-03-08 20:16:56 +0100

    uncomment home graphe

--- Startup.cs
+++ Startup.cs
@@ -85,9 +85,9 @@ namespace Admin5
 
             services.AddDbContext<AdminContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("didh"));
+                // options.UseSqlServer(Configuration.GetConnectionString("didh"));
                 // options.UseSqlServer(Configuration.GetConnectionString("db"));
-                // options.UseSqlite(Configuration.GetConnectionString("sqlite"));
+                options.UseSqlite(Configuration.GetConnectionString("sqlite"));
                 // options.EnableSensitiveDataLogging();
             });
 
